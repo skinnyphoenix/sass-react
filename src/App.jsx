68fd0fb2ad3ebaf0e39d909652985564c5cbcd55
Giovanni <skinnyphoenix@icloud.com>
2023-07-04 17:23:38 +0200

    04-07-2023

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,57 @@ function App() {
   return (
     <>
       <div className="container">
-        <ul className="list1">
+        <div className="headings__">
+          <h1 className="heading1">Heading 1</h1>
+          <h2 className="heading2">Heading 2</h2>
+          <p className="paragraph__test">Paragraph</p>
+          <div className="wrapper">Element max width test</div>
+        </div>
+
+        {/* <ul className="list1">
           <li>Lista 1</li>
           <li>Lista 2</li>
-        </ul>
+        </ul> */}
       </div>
-      <div className="wrapper">Element max width test</div>
+      <div className="container__column">
+        <div className="wrapperColumn">
+          <div className="col-1">Col-1</div>
+          <div className="col-1">Col-1</div>
+          <div className="col-1">Col-1</div>
+          <div className="col-1">Col-1</div>
+          <div className="col-1">Col-1</div>
+          <div className="col-1">Col-1</div>
+          <div className="col-1">Col-1</div>
+          <div className="col-1">Col-1</div>
+          <div className="col-1">Col-1</div>
+          <div className="col-1">Col-1</div>
+        </div>
+        <div className="wrapperColumn__2">
+          <div className="col-2">Col-2</div>
+          <div className="col-2">Col-2</div>
+          <div className="col-2">Col-2</div>
+          <div className="col-2">Col-2</div>
+          <div className="col-2">Col-2</div>
+          <div className="col-2">Col-2</div>
+          <div className="col-2">Col-2</div>
+          <div className="col-2">Col-2</div>
+          <div className="col-2">Col-2</div>
+          <div className="col-2">Col-2</div>
+        </div>
+
+        <div className="wrapperColumn__3">
+          <div className="col-3">Col-3</div>
+          <div className="col-3">Col-3</div>
+          <div className="col-3">Col-3</div>
+          <div className="col-3">Col-3</div>
+          <div className="col-3">Col-3</div>
+          <div className="col-3">Col-3</div>
+          <div className="col-3">Col-3</div>
+          <div className="col-3">Col-3</div>
+          <div className="col-3">Col-3</div>
+          <div className="col-3">Col-3</div>
+        </div>
+      </div>
     </>
   );
 }
